a3ff99a4854785e19fdc033aa13a89a7f9b43593
Ifeanyi <46653710+iecheta@users.noreply.github.com>
2022-06-05 01:15:04 +0100

    added install function

---
+++ functions/latticelibinstall.m
@@ -0,0 +1,9 @@
+function latticelibinstall()
+
+% Add LatticeLib's folders to the MATLAB search path
+
+addpath(genpath('..\exampledata'))
+addpath(genpath('..\functions'))
+addpath(genpath('..\scripts'))
+savepath
+disp('LatticeLib folders added to MATLAB searach path.')
